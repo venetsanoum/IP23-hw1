79737c58349da654a7191e81059ae208aaf5826f
venetsanoum <sdi2300016@di.uoa.gr>
2023-12-03 20:27:18 +0200

    mirror.c

--- mirror/src/mirror.c
+++ mirror/src/mirror.c
@@ -6,7 +6,7 @@ int prime (unsigned long long n){ //Function to check if a number n is prime.
     if (n == 1) { //1 is not prime
         return 1;
     }
-    if (n == 2 || n == 3) { // 2 and 3 are prime numbers 
+    if (n == 2 || n == 3) { // 2 and 3 are prime numbers
         return 0;
     }
 
@@ -16,12 +16,12 @@ int prime (unsigned long long n){ //Function to check if a number n is prime.
 
     for (unsigned long long i = 5; i * i <= n; i+=6){ //Starting from 5 because 4 is not prime and cases 1,2,3 have been checked.
 
-         // n % i == 0: This checks if n is divisible by the value of i. 
-        // n % (i + 2) == 0: This checks if n is divisible by i + 2, which is essentially checking for divisibility by the next possible divisor after i.               
+         // n % i == 0: This checks if n is divisible by the value of i.
+        // n % (i + 2) == 0: This checks if n is divisible by i + 2, which is essentially checking for divisibility by the next possible divisor after i.
 
         if (n % i == 0 || n % (i + 2) == 0) { //for loop continues until i*i>n. Because if it has a divisor larger
-            return 1;     //than its square root, it must also have a corresponding factor smaller than its square root, which means that the number is not prime. 
-                          
+            return 1;     //than its square root, it must also have a corresponding factor smaller than its square root, which means that the number is not prime.
+
         }
     }
     return 0;
@@ -41,7 +41,7 @@ unsigned long long mirror (unsigned long long n) { //Function that returns the m
 
  int perfectsquare(unsigned long long  n){ //Function to check if a number n is a perfect square.
     unsigned long long root=(long long)(sqrt(n));
-        return root*root==n;   
+        return root*root==n;
 }
 
 
@@ -50,8 +50,8 @@ int main (int argc, char **argv) {
     if (argc != 3) {
         printf("Program needs to be called as: ./mirror lower upper\n"); //If the number of arguments is not correct the program terminates
         return 1;
-    } 
-    
+    }
+
     unsigned long long lower = atoll(argv[1]);
     unsigned long long upper = atoll(argv[2]);
 
@@ -60,23 +60,23 @@ int main (int argc, char **argv) {
             return 1;
         }
 
-    
+
     unsigned long long sum = 0; //Initialize the sum
     for (unsigned long long i = (long double)sqrt(lower); i <= (long double)sqrt(upper); i++) {
         //Creating the squares that will be added in the sum. (i = (long double)sqrt(lower); i <= (long double)sqrt(upper);)
-            if (prime(i) == 0 ) { //Check if i is prime
+            if (prime(i)== 0 ) { //Check if i is prime
                 unsigned long long mirrornumber = mirror(i*i);
-                if ( (i*i) != mirrornumber )   {//check if squared i and its mirror are not palindromic 
-                    if (prime ((long double)sqrt(mirrornumber == 0))){ //check if the square root of mirror number is prime
-                        if (perfectsquare(i*i)  && perfectsquare(mirrornumber)  ){ 
-                        //check if i squared is perfect square, if mirror  i squared is perfect square and 
-                            sum += i*i; 
+                if ( (i*i) != mirrornumber )   {//check if squared i and its mirror are not palindromic
+                    if (prime ((long double)sqrt(mirrornumber)) == 0){ //check if the square root of mirror number is prime
+                        if (perfectsquare(i*i)  && perfectsquare(mirrornumber)  ){
+                        //check if i squared is perfect square, if mirror  i squared is perfect square and
+                            sum += i*i;
                         }
                     }
                 }
             }
-        
+
     }
-    printf("%lld\n" ,sum); 
+    printf("%lld\n" ,sum);
     return 0;
 }
